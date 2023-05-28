1a0ffedd1f4e722fd18b8f768830aa67807714b5
PlatenPrime <proshta@gmail.com>
2023-05-28 20:15:47 +0300

    28.05.2023

--- 02_BasicTypes/build/js/main.js
+++ 02_BasicTypes/build/js/main.js
@@ -1,8 +1,15 @@
 "use strict";
-let username = " Platen Prime";
-console.log(username);
-let a = 12;
-let b = '6';
-let c = 2;
-console.log(a / b);
-console.log(c * b);
+let myName = "Platen";
+let meaningOfLife;
+let isLoading;
+let album;
+myName = "Prime";
+meaningOfLife = 42;
+isLoading = true;
+album = 5150;
+const sum = (a, b) => {
+    return a + b;
+};
+let postId;
+let isActive;
+let re = /\w+/g;
---
+++ 02_BasicTypes/src/main.ts
@@ -0,0 +1,20 @@
+let myName: string = "Platen"
+let meaningOfLife: number;
+let isLoading: boolean;
+let album: string | number;
+
+myName = "Prime"
+meaningOfLife = 42
+isLoading = true
+album = 5150
+
+
+
+const sum = (a: number, b: string) => {
+    return a + b
+}
+
+let postId: string | number
+let isActive: number | boolean
+
+let re: RegExp = /\w+/g
