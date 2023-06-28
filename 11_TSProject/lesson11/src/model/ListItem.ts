70a0e5e65f788a0be05df235ccfffab51799137d
PlatenPrime <proshta@gmail.com>
2023-06-28 08:28:18 +0300

    28.06.2023

---
+++ 11_TSProject/lesson11/src/model/ListItem.ts
@@ -0,0 +1,43 @@
+export interface Item {
+	item: string,
+	item: string,
+	checked: boolean,
+}
+
+export default class ListItem implements Item {
+	constructor(
+		private _id: string = "",
+		private _item: _string = "",
+		private _checked: boolean = false,
+	) { }
+
+	get id(): string {
+		return this._id
+	}
+
+	set id(id: string) {
+		this._id = id
+	}
+
+
+	get item(): string {
+		return this._item
+	}
+
+	set item(item: string) {
+		this._item = item
+	}
+
+
+	get checked(): boolean {
+		return this.checked
+	}
+
+	set checked(checked: boolean) {
+		this.checked = checked
+	}
+
+
+
+
+}
