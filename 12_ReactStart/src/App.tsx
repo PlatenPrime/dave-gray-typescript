01bbea7b3651eb148dc7f0abfeb810fe3fb66804
PlatenPrime <proshta@gmail.com>
2023-07-03 09:00:20 +0300

    03.07.2023

---
+++ 12_ReactStart/src/App.tsx
@@ -0,0 +1,17 @@
+import Heading from "./components/Heading"
+import Section from "./components/Section"
+
+function App() {
+
+
+	return (
+		<>
+			<Heading title="Hello" ></Heading>
+			<Section title="My personal subheading" >
+				This is my section
+			</Section>
+		</>
+	)
+}
+
+export default App
