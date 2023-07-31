8e1e8ab453852f58471a81033e794d4a9c1c737d
PlatenPrime <proshta@gmail.com>
2023-07-31 09:06:47 +0300

    31.07.2023

--- Documentation/hello.js
+++ Documentation/hello.js
@@ -1,5 +1,20 @@
+"use strict";
 console.log("hello world");
 function greet(person, date) {
-    console.log(`Hello ${person}, today is ${date.toDateString()}!`);
+    console.log("Hello ".concat(person, ", today is ").concat(date.toDateString(), "!"));
 }
 greet("John", new Date());
+var arrNumber = [1, 2];
+var arrNumber2 = ["1", "2"];
+var myName = "Platen";
+function greetNew(name) {
+    console.log("Hello, " + name.toUpperCase() + "!!");
+}
+greetNew("John");
+function double(num) {
+    return num * 2;
+}
+var palletSome = function (pallet) {
+    console.log(pallet.position);
+    console.log(pallet.pieces);
+};
--- Documentation/hello.ts
+++ Documentation/hello.ts
@@ -1,3 +1,5 @@
+"use strict"
+
 console.log("hello world")
 
 function greet(person: string, date: Date): void {
@@ -6,3 +8,24 @@ function greet(person: string, date: Date): void {
 
 greet("John", new Date());
 
+let arrNumber: number[] = [1, 2];
+let arrNumber2: Array<string> = ["1", "2"];
+
+const myName: string = "Platen";
+
+
+function greetNew(name: string): void {
+	console.log("Hello, " + name.toUpperCase() + "!!");
+}
+
+greetNew("John")
+
+
+function double(num: number): number {
+	return num * 2;
+}
+
+const palletSome = (pallet: { position: string, pieces: string }): void => {
+	console.log(pallet.position)
+	console.log(pallet.pieces)
+}
