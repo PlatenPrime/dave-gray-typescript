0ab33ff904f2ea9ccf49beca6b983e6c928f2478
PlatenPrime <proshta@gmail.com>
2024-04-01 20:05:28 +0300

    01.04.2024

--- lamadev/lamadev-next-ts/src/app/SecondChild.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function SecondChild() {
-	return (
-		<div>SecondChild</div>
-	)
-}
-
-export default SecondChild
---
+++ src/App.tsx
@@ -0,0 +1,32 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+	const message: string = "Hello World!";
+
+
+	message.toLowerCase();
+	// Calling 'message'
+	message();
+
+
+
+
+
+
+	function fn(x) {
+		return x.flip();
+	}
+
+	return (
+		<>
+
+
+		</>
+	)
+}
+
+export default App
