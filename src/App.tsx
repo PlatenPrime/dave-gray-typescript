50a7e65287743f0748e4580ec67c6c5cc3e21247
Aleksandr Marchenko <proshta@gmail.com>
2024-04-09 10:26:06 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -14,12 +14,65 @@ function App() {
 
 
 
+	const user = {
+		name: "Daniel",
+		age: 26,
+	};
+	user.location;
+
 
 
 
 	function fn(x) {
 		return x.flip();
 	}
+
+
+
+	const announcement: string = "Hello World!";
+
+	// How quickly can you spot the typos?
+	announcement.toLocaleLowercase();
+	announcement.toLocalLowerCase();
+
+	// We probably meant to write this...
+	announcement.toLowerCase();
+
+
+
+
+
+
+	function flipCoin() {
+		// Meant to be Math.random()
+		return Math.random() < 0.5;
+
+	}
+
+
+	const value = Math.random() < 0.5 ? "a" : "b";
+	if (value !== "a") {
+		// ...
+	} else if (value === "b") {
+	
+		// Oops, unreachable
+	  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 	return (
 		<>
